55eaac07a930e0c019f5db7097f3b6235d5175c2
gussann <84413122+gussann@users.noreply.github.com>
2021-07-11 19:31:53 +0900

    ログアウト状態のユーザーをログインページにリダイレクトするように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -54,7 +54,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :index
+      redirect_to '/users/sign_in'
     end
   end
 end
